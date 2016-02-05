4b2c6d497c8e743585befa925fa992bfee01a2b3
gabriel7706 <hall18@up.edu>
2016-02-04 16:06:46 -0800

    GABES COMMIT

--- app/src/main/java/edu/up/cs371/textmod/TextModActivity.java
+++ app/src/main/java/edu/up/cs371/textmod/TextModActivity.java
@@ -5,6 +5,7 @@ package edu.up.cs371.textmod;
  *
  * Allow text to be modified in simple ways with button-presses.
  */
+import android.content.DialogInterface;
 import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -16,11 +17,19 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import java.util.ArrayList;
 
-public class TextModActivity extends ActionBarActivity {
+public class TextModActivity extends ActionBarActivity implements View.OnClickListener {
+
+    protected Button upperButton;
+    protected EditText editText;
+
+
+
 
     // array-list that contains our images to display
     private ArrayList<Bitmap> images;
@@ -68,6 +77,12 @@ public class TextModActivity extends ActionBarActivity {
             // load the image; add to arraylist
             Bitmap img = BitmapFactory.decodeResource(getResources(), id);
             images.add(img);
+
+            upperButton =(Button) findViewById(R.id.upperButton);
+            upperButton.setOnClickListener(this);
+            editText =(EditText) findViewById(R.id.editText);
+
+
         }
 
         // define a listener for the spinner
@@ -101,6 +116,19 @@ public class TextModActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if( v == upperButton){
+
+          String userText = editText.getText().toString();
+            userText = userText.toUpperCase();
+            editText.setText(userText);
+
+
+        }
+
     }
 
     /**
